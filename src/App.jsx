f702ac59a0477eb2857d73758710a0ea0c98e25c
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2025-02-15 00:20:28 +0100

    set up general css and appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation, useNavigationType } from "react-router-dom";
+import SignUp from "./component/signup";
+import Login from "./component/login";
+import DummyPage from "./component/dummy";
+import UnderwaterBackground from "./component/UnderwaterBackground";
+import Preloader from "./component/loader";
 
-function App() {
-  const [count, setCount] = useState(0)
+const AppWrapper = () => {
+  const location = useLocation();
+  const navigationType = useNavigationType();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 6000); 
+  }, []);
+
+  useEffect(() => {
+    if (navigationType !== "POP") {
+      setLoading(true);
+      setTimeout(() => setLoading(false), 6000); 
+    }
+  }, [location]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {loading && <Preloader />}
+      {!loading && (
+        <>
+          <UnderwaterBackground />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dummy" element={<DummyPage />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+const App = () => (
+  <Router>
+    <AppWrapper />
+  </Router>
+);
+
+export default App;
